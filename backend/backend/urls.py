02e651894b442ea352aedc364614f275e7c5d11b
preetgur <preetgur0137@gmail.com>
2021-03-30 15:04:58 +0530

    serialize the data

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('base.urls')),  # included our app
+    path('',include('base.urls')),  # included our app
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- backend/base/views.py
+++ backend/base/views.py
@@ -3,7 +3,8 @@ from django.http import JsonResponse
 from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import ProductSerializer
+from .models import Product
 # Create your views here.
 
 def home(request):
@@ -12,16 +13,22 @@ def home(request):
 
 @api_view(['GET'])
 def getProducts(request):
+    products = Product.objects.all()
+    serialzer = ProductSerializer(products,many=True) 
+
    
-    return Response(products)
+    return Response(serialzer.data)
 
 @api_view(['GET'])
 def getProduct(request,pk):
-    
-    product = None
-    for i in products:
-       if  i["_id"] == pk:
-           product = i
-           break
+
+    product = Product.objects.get(_id = pk)
+    serialzer = ProductSerializer(product,many=False) 
+
+    # product = None
+    # for i in products:
+    #    if  i["_id"] == pk:
+    #        product = i
+    #        break
    
-    return Response(product)    
+    return Response(serialzer.data)    
