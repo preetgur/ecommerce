961c711b51a5fdaa90816189f732e86c5096d20b
preetgur <preetgur0137@gmail.com>
2021-03-30 11:52:16 +0530

    get the static data from api

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,31 @@
-import React from 'react'
-import productList from '../products'   
+import React, { useEffect, useState } from 'react'
+// import productList from '../products'   
 import Product from '../components/Product'
 import './HomeScreen.css'
+import axios from "axios"
 
 function HomeScreen() {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+            
+        async function fetchProduct() {
+            
+           const {data} = await axios.get('http://127.0.0.1:8000/getProducts/')
+            setProducts(data)
+           
+        }
+        fetchProduct()
+
+    }, [])
+
     return (
         <div className="homeScreen">
             <h1>Latest Products</h1>
             <div className="homeScreen__latestProducts">
 
-                {productList.map( product => ( 
+                {products.map( product => ( 
                         
                         <Product key={product._id} product={product}/>
                      
