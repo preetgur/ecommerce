ea7b455024b50b3e616ab343b7e4248f35646eef
preetgur <preetgur0137@gmail.com>
2021-04-04 16:46:48 +0530

    JsonWeb token installatiion  and customizations

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+   
+)
 
 urlpatterns = [
     path('',views.home , name='home'),
     path('getProducts/',views.getProducts,name="getProducts"),
     path('getProduct/<str:pk>/',views.getProduct,name="getProduct"),
 
+    path('api/user/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
