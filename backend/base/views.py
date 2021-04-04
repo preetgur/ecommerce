b9e34e5dff3110671464702399c4d26141ebd01b
preetgur <preetgur0137@gmail.com>
2021-04-04 21:12:20 +0530

    JWt customization Second Phase In order to get the username and email with token

--- backend/base/views.py
+++ backend/base/views.py
@@ -11,17 +11,18 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
 
-        # Add custom claims
-        token['username'] = user.username
-        token['email'] = user.email
+    # overrides the validate method
+   def validate(self,attrs):
+       print("attrs .. ",attrs)
+       data = super().validate(attrs)
 
-        # ...
+       # get username and email 
+       data['username'] = self.user.username
+       data['email'] = self.user.email
+       
+       return data
 
-        return token
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -50,4 +51,4 @@ def getProduct(request,pk):
     #        product = i
     #        break
    
-    return Response(serialzer.data)    
+    re
