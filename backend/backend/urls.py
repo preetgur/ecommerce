d5a05c1d6c4b87cc5990dff20ea89981ed6de0e4
preetgur <preetgur0137@gmail.com>
2021-04-27 14:04:18 +0530

    react+Django intergrations

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,14 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     # path('',include('base.urls')),  # included our app
 
     path('api/users/',include('base.urls.user_urls')),  # included user urls
     path('api/products/',include('base.urls.product_urls')),  
     path('api/orders/',include('base.urls.order_urls')),  
+    path("", TemplateView.as_view(template_name="index.html")),
 
 
 
